e5880bf200aa3e12c07397a80d42c61f474c3cb8
Ben Arnold <ben@arnoldfamily.me.uk>
2017-09-07 14:52:56 +0100

    An implementation of ThrottleAfter

--- Omichron/TimeLogsView.xaml.cs
+++ Omichron/TimeLogsView.xaml.cs
@@ -87,4 +87,35 @@ namespace Omichron
         IScreen IRoutableViewModel.HostScreen => hostScreen;
 
     }
+
+    public static class ObservableExtensions
+    {
+        /// <summary>
+        /// Ignores elements of an observable sequence that follow other elements within a specified relative time duration.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements in the source sequence.</typeparam>
+        /// <param name="source">Sequence to throttle.</param>
+        /// <param name="sampleDuration">Throttling </param>
+        /// <returns>The throttled sequence.</returns>
+        /// <exception cref="ArgumentNullException">source is null.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">sampleDuration is less than TimeSpan.Zero.</exception>
+        /// <remarks>This operator throttles the source sequence by creating a <see cref="Window"/> over the sequence
+        /// and taking only the first element in the window.  As the element is produced immediately, even sequences
+        /// with gaps equal or larger than <paramref name="sampleDuration"/> between elements will produce the element.
+        /// This is unlike <see cref="Throttle"/> which does not produce any elements in that case.
+        /// </remarks>
+        public static IObservable<T> ThrottleAfter<T>(this IObservable<T> source, TimeSpan sampleDuration)
+        {
+            return source
+                .Window(() => Observable.Interval(sampleDuration))
+                .SelectMany(x => x.Take(1));
+        }
+
+        public static IObservable<T> ThrottleAfter<T>(this IObservable<T> source, TimeSpan sampleDuration, IScheduler scheduler)
+        {
+            return source
+                .Window(() => Observable.Interval(sampleDuration, scheduler))
+                .SelectMany(x => x.Take(1));
+        }
+    }
 }
---
+++ Tests/Class1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using Omichron;
+using Microsoft.Reactive.Testing;
+using Ploeh.AutoFixture.Xunit2;
+using ReactiveUI;
+using Xunit;
+
+namespace Tests
+{
+    public class TestThrottleAfter
+    {
+        [Theory, AutoData]
+        public void WhenNoSequence_NothingObserved(TestScheduler scheduler, Subject<char> subject, long delay)
+        {
+            var actual = subject.AsObservable()
+                .ThrottleAfter(TimeSpan.FromMilliseconds(delay), scheduler)
+                .CreateCollection();
+
+            scheduler.AdvanceTo(delay + 1);
+
+            Assert.Empty(actual);
+        }
+
+        [Theory, AutoData]
+        public void WhenSingleElement_ItIsObserved(TestScheduler scheduler, Subject<char> subject, long delay, char c)
+        {
+            var expected = new[] { c };
+
+            var actual = subject.AsObservable()
+                .ThrottleAfter(TimeSpan.FromMilliseconds(delay), scheduler)
+                .CreateCollection();
+
+            scheduler.Schedule(TimeSpan.FromMilliseconds(0), () => subject.OnNext(c));
+            scheduler.AdvanceTo(delay + 1);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory, AutoData]
+        public void WhenTwoElementsThatAreFarApart_BothAreObserved(TestScheduler scheduler, Subject<char> subject, long delay, char a, char b)
+        {
+            var expected = new[] { a, b };
+
+            var actual = subject.AsObservable()
+                .ThrottleAfter(TimeSpan.FromMilliseconds(delay), scheduler)
+                .CreateCollection();
+
+            scheduler.Schedule(TimeSpan.FromMilliseconds(0), () => subject.OnNext(a));
+            scheduler.Schedule(TimeSpan.FromMilliseconds(delay + 1), () => subject.OnNext(a));
+            scheduler.AdvanceTo(delay + 2);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+
+    static class ScheduleExtensions
+    {
+        public static IDisposable Schedule<TAbsolute, TRelative, TState>(this VirtualTimeSchedulerBase<TAbsolute, TRelative> scheduler, TState state, Action action)
+            where TAbsolute : IComparable<TAbsolute>
+        {
+            return scheduler.Schedule(state, (a, b) =>
+            {
+                action();
+                return Disposable.Empty;
+            });
+        }
+    }
+}
