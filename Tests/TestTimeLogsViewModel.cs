6402cfbdff2dc9dc49ddc47ee0358a6860ee1099
Ben Arnold <ben@arnoldfamily.me.uk>
2017-09-25 03:46:04 +0100

    DesignTimeViewModel in terms of real view model

--- Tests/TestTimeLogsViewModel.cs
+++ Tests/TestTimeLogsViewModel.cs
@@ -12,42 +12,50 @@ using Xunit;
 
 namespace Tests
 {
-    public class TestTimeLogsViewModel
+    namespace TimeLogsViewModelTests
     {
-        // This isn't apparently needed, leaving it for a while just in case!
-        //public TestTimeLogsViewModel()
-        //{
-        //    var x = RxApp.MainThreadScheduler;
-        //    //RxApp.MainThreadScheduler = System.Reactive.Concurrency.ImmediateScheduler.Instance;
-        //}
-
-        [Theory, AutoMockData]
-        public void Logs_are_empty_after_construction(TimeLogsViewModel sut, [Frozen] List<TimeLog> logs, TimeLogSource api)
+        public class TestTimeLogsViewModel
         {
-            ((ISupportsActivation)sut).Activator.Activate();
-            sut.Logs.Should().BeEmpty();
-        }
+            // This isn't apparently needed, leaving it for a while just in case!
+            //public TestTimeLogsViewModel()
+            //{
+            //    var x = RxApp.MainThreadScheduler;
+            //    //RxApp.MainThreadScheduler = System.Reactive.Concurrency.ImmediateScheduler.Instance;
+            //}
 
-        [Theory, AutoMockData]
-        public void Logs_are_retrieved_when_search_is_executed(TimeLogsViewModel sut, [Frozen] List<TimeLog> expected, TimeLogSource api)
-        {
-            // Given
-            ((ISupportsActivation)sut).Activator.Activate();
-            // When
-            sut.ExecuteSearch.Execute().Subscribe();
-            // Then
-            sut.Logs.Should().BeEquivalentTo(expected);
-        }
+            [Theory, AutoMockData]
+            public void Logs_are_empty_after_construction(TimeLogsViewModel sut, [Frozen] List<TimeLog> logs, TimeLogSource api)
+            {
+                ((ISupportsActivation)sut).Activator.Activate();
+                sut.Logs.Should().BeEmpty();
+            }
 
-        [Theory, AutoMockData]
-        public void Logs_are_retrieved_when_application_is_activated([Frozen] IApplicationEvents events, TimeLogsViewModel sut, [Frozen] List<TimeLog> expected, TimeLogSource api)
-        {
-            // Given
-            events.Activated.Returns(x => Observable.Return(new EventArgs()));
-            // When
-            ((ISupportsActivation)sut).Activator.Activate();
-            // Then
-            sut.Logs.Should().BeEquivalentTo(expected);
+            [Theory, AutoMockData]
+            public void Logs_are_retrieved_when_search_is_executed(TimeLogsViewModel sut, [Frozen] List<TimeLog> expected, TimeLogSource api)
+            {
+                // Given
+                ((ISupportsActivation)sut).Activator.Activate();
+                // When
+                sut.ExecuteSearch.Execute().Subscribe();
+                // Then
+                sut.Logs.Should().BeEquivalentTo(expected);
+            }
+
+            [Theory, AutoMockData]
+            public void Logs_are_retrieved_when_application_is_activated([Frozen] IApplicationEvents events, TimeLogsViewModel sut, [Frozen] List<TimeLog> expected, TimeLogSource api)
+            {
+                // Given
+                events.Activated.Returns(x => Observable.Return(new EventArgs()));
+                // When
+                ((ISupportsActivation)sut).Activator.Activate();
+                // Then
+                sut.Logs.Should().BeEquivalentTo(expected);
+            }
+
+            [Theory, AutoMockData]
+            public void Logs_are_restricted_to_after_start_date([Frozen] IApplicationEvents events, TimeLogsViewModel sut, [Frozen] List<TimeLog> expected, TimeLogSource api)
+            {
+            }
         }
     }
 }
